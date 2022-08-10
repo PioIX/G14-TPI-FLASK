7a2a9a187059442bdf4928ea36b93dc6d6c13f07
Loco 5484 <100932334+IvanLSanchez@users.noreply.github.com>
2022-08-10 11:27:00 +0000

    progreswo index

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  conn = sqlite3.connect('ODSGames.db')
+  
+  a = '''SELECT titulo FROM juegos WHERE ods=2'''
+  b = '''SELECT titulo FROM juegos WHERE ods=5'''
+  c = '''SELECT titulo FROM juegos WHERE ods=7'''
+    
+  return render_template('index.html', tituloODSDos=conn.execute(a).fetchone()[0], tituloODSCinco=conn.execute(b).fetchone()[0], tituloODSSiete=conn.execute(c).fetchone()[0])
+
 
 @app.route('/sorry')
 def paginaNoCreada():
