007b8bfd2f7cbee63227b4fc7520f42398322c92
Loco 5484 <100932334+IvanLSanchez@users.noreply.github.com>
2022-08-12 15:44:47 +0000

    a

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ def index():
     
   return render_template('index.html', tituloODSDos=conn.execute(a).fetchone()[0], comoSeJuegaODSDos=conn.execute(aa).fetchone()[0], tituloODSCinco=conn.execute(b).fetchone()[0], comoSeJuegaODSCinco=conn.execute(bb).fetchone()[0], tituloODSSiete=conn.execute(c).fetchone()[0])
 
+@app.route('/loginin')
+def iniciarSesion():
+  return render_template('inicioSesion.html')
 
+@app.route('/singin')
+def registrarse():
+  return render_template('registrarse.html')
+  
 @app.route('/sorry')
 def paginaNoCreada():
   return render_template('paginaNoCreada.html')
